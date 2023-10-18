b5d8130ad27e9fe8d45ba87729a3ecf9e62184a4
demessietry <104842551+demessietry@users.noreply.github.com>
2023-10-18 21:51:04 +0300

    Edit style for small size media

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ export default function Header() {
         className="header--image"
       />
       <h2 className="header--title">Meme Generator</h2>
-      <h4 className="header--project">React Course - Project Meme</h4>
+      <h4 className="header--project">React - Meme Project </h4>
     </header>
   );
 }
